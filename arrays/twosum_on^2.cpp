d519a98d231428a19a5794aa99a10172095270e5
Your Name <animusaaa@gmail.com>
2025-06-26 18:20:41 +0530

    twosum

---
+++ arrays/twosum_on^2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+pair<int,int> twosum(const vector<int>&nums,int target){
+  for(int i =0;i<nums.size();i++){
+    for(int j=i+1;j<nums.size();j++){
+      if(nums[i]+nums[j]== target){
+        return {i,j};
+      }
+    }
+  }
+  return {-1,-1};
+}
+int main(){
+  vector<int> nums = {1,2,4,5,6,7};
+  int target = 13;
+  pair<int,int> result = twosum(nums,target);
+  if(result.first!=-1){
+    cout<<result.first<<","<<result.second<<endl;
+  }
+  else{
+    cout<<"no pair found"<<endl;
+  }
+}
